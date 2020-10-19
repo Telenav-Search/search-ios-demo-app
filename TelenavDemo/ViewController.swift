c52186311e59cc9ce868210e58d89c70251d14fb
Egor Zaderiy <ezaderiy@lineate.com>
2020-10-19 15:21:09 +0600

    Demo start

---
+++ TelenavDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TelenavDemo
+//
+//  Created by ezaderiy on 19.10.2020.
+//
+
+import UIKit
+import TelenavSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
