ac832d21d3a7b25984f0664f72a936d68f047ce0
Valeria Mozghova <valeriia.mozghova2012@gmail.com>
2020-11-23 14:19:29 +0200

    Fix displaying distance number

--- TelenavDemo/Cells/SearchResultCell.swift
+++ TelenavDemo/Cells/SearchResultCell.swift
@@ -31,7 +31,7 @@ class SearchResultCell: UITableViewCell {
     }
 
     func fillSearchResultItem(_ item: TelenavEntity, itemNumber: Int) {
-        self.numberLabel.text = "\(itemNumber)"
+        self.numberLabel.text = "\(itemNumber)."
         self.nameLabel.text = item.place?.name
         self.addressLabel.text = item.place?.address?.formattedAddress
         
