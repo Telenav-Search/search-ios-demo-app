6caaecd3f2956b943a829e71b5fe8a3eefd6cf08
Egor Zaderiy <ezaderiy@lineate.com>
2020-12-11 20:53:18 +0600

    Updated objc example

--- TelenavDemo/Objective-C example/ObjcExample.m
+++ TelenavDemo/Objective-C example/ObjcExample.m
@@ -13,11 +13,36 @@
 
 - (void)test {
     
-    TNEntitySearchParams *params = [[[[TNEntitySearchQueryBuilder new] query:@"food"] location:[TNEntityGeoPoint pointWithLat:0 lon:0] ] build];
+//    TNEntitySearchParams *params = [[[[TNEntitySearchQueryBuilder new] query:@"food"] location:[TNEntityGeoPoint pointWithLat:0 lon:0] ] build];
+//
+//    [TNEntityCore searchWithSearchParams:params completion:^(TNEntitySearchResult * _Nullable res, NSError * _Nullable err) {
+//
+//    }];
     
-    [TNEntityCore searchWithSearchParams:params completion:^(TNEntitySearchResult * _Nullable res, NSError * _Nullable err) {
-        
-    }];
+//    TNEntityPredictionWordParams *params = [[TNEntityPredictionWordParams alloc]
+//                                            initWithSearchQuery:@""
+//                                            location:[[TNEntityGeoPoint alloc] initWithLat:33 lon:143 ]];
+//    [TNEntityCore getWordPredictionsWithParams:params completion:^(TNEntityPredictionWordResult * _Nullable res, NSError * _Nullable err) {
+//            NSLog(@"%@", res.results);
+//    }];
+//
+//    TNEntitySuggestionParams *params = [[TNEntitySuggestionParams alloc]
+//                                        initWithSearchQuery:@"mc"
+//                                        location:[[TNEntityGeoPoint alloc] initWithLat:37 lon:-122 ]
+//                                        includeEntity:true];
+//    [TNEntityCore getSuggestionsWithParams:params completion:^(TNEntitySuggestionResult * _Nullable res, NSError * _Nullable err) {
+//        NSLog(@"%@", res.results);
+//    }];
+//
+//
+//    NSString *entityID = @"Yz1zYW50YSBjbGFyYTtjbz11cztzZm49Z3JlYXQgYW1lcmljYSBwa3d5O2g9NDY1NTt6PTk1MDU0O3M9Y2E7aWQ9MTA2NTI0MDYxO2x0PTM3LjM5ODE1O2xuPS0xMjEuOTc3ODk7cmdjPWZhbHNlOwV6";
+//    TNEntityDetailOptions *detailOpt = [[TNEntityDetailOptions alloc] initWithDetailLevel:DetailLevelFull showAddressLines:YES];
+//
+//    TNEntityParams *params = [[TNEntityParams alloc] initWithIds:@[] detailOptions:detailOpt facetParams:nil];
+//    [TNEntityCore getEntityDetailsWithParams:params completion:^(NSArray<TNEntity *> * _Nullable res, NSError * _Nullable err) {
+//        NSLog(@"%@", res);
+//    }];
+    
 }
 
 @end
