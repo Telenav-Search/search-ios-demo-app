c7b34a4ec0763fca300f06ab0dd3b499d8335743
AnatolyDeveloper <61194847+AnatolyDeveloper@users.noreply.github.com>
2021-11-10 14:06:41 +0300

    TVN-177: Implement navigation session to Demo app (#40)
    * TVN-177: In progress
    
    * TVN-177: Some changes
    
    Co-authored-by: AnatolYarmolovich <yarmolovich1986@gmail.com>

---
+++ TelenavDemo/ViewControllers/TelenavMap/RouteCell/RouteCell.swift
@@ -0,0 +1,23 @@
+//
+//  RouteCell.swift
+//  TelenavDemo
+//
+//  Created by Anatol Uarmolovich on 9.11.21.
+//
+
+import UIKit
+
+class RouteCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override var isSelected: Bool {
+        didSet {
+            self.backgroundColor = isSelected ? .systemRed : .systemBlue
+        }
+    }
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+}
